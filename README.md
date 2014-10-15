9f5157820ec294a02bfc4d1297cbd9d8a75a6e41
mkao006 <mkao006@gmail.com>
2014-10-15 14:34:41 +0200

    First commit after moving repository

---
+++ README.md
@@ -0,0 +1,15 @@
+# FAOSWS Utility package
+
+This is an add-on utilities package to the **faosws** package 
+
+### Installation
+
+To install the imputation package, simply run the following command. 
+
+```r
+library(devtools)
+
+## The package depends on the faoswsFlag package
+install_github(repo = "sws_util", username = "mkao006", 
+               subdir = "faoswsUtil")
+```
---
+++ buildPackage.R
@@ -0,0 +1,29 @@
+## Building the package
+## ---------------------------------------------------------------------
+
+library(roxygen2)
+
+## Remove the folder if it exists
+if(file.exists("./faoswsUtil"))
+    unlink("faoswsUtil", recursive = TRUE)
+
+## Build the package
+package.skeleton("faoswsUtil", code_files = paste("./codes/",
+                           dir("./codes/", pattern = "\\.R$"), sep = ""),
+                 force = FALSE)
+
+
+## Include the DESCRIPTION file
+file.copy(from = "./DESCRIPTION", to = "faoswsUtil/",
+          overwrite = TRUE)
+unlink("./faoswsUtil/Read\\-and\\-delete\\-me")
+
+## Use roxygen to build the documentation
+roxygenize("faoswsUtil")
+unlink("./faoswsUtil/inst/", recursive = TRUE)
+
+## Build and check the package
+system("R CMD INSTALL --build faoswsUtil")
+system("R CMD build faoswsUtil")
+## system("R CMD check --as-cran faoswsUtil")
+
