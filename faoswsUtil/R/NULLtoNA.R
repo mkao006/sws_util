a22c25465f361add325320c458aaf814683a1f64
mkao006 <mkao006@gmail.com>
2014-11-28 12:26:28 +0100

    Updated package

--- faoswsUtil/R/NULLtoNA.R
+++ faoswsUtil/R/NULLtoNA.R
@@ -6,8 +6,25 @@
 ##' @param nullList The list of NULL
 ##'
 ##' @export
+
 NULLtoNA = function(nullList){
-    vector = rep(NA, length = length(nullList))
+    ## Assuming the list contains only one type and NULL
+    listType =
+        unique(na.omit(sapply(nullList,
+                              FUN = function(x){
+                                  ifelse(is.null(x), NA, typeof(x))
+                              }
+                              )
+                       )
+               )
+    ## NOTE (Michael): Default the column to character, the reasoning
+    ##                 is because most of the column which has all of
+    ##                 its value as NULL usually belong to the flag
+    ##                 cloumn.
+    if(length(listType) == 0)
+        listType = "character"
+    vector = as.vector(rep(NA, length = length(nullList)),
+        mode = listType)
     validEntry = which(sapply(nullList, FUN = function(x) !is.null(x)))
     vector[validEntry] =
         unlist(nullList[validEntry])
