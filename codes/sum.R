97d8aa1ef25525b92a43ae6a61d29011c045ce3d
mkao006 <mkao006@gmail.com>
2014-12-06 14:31:37 +0100

    updated comment

--- codes/sum.R
+++ codes/sum.R
@@ -2,9 +2,12 @@
 ##'
 ##'
 ##' The function sums a vector with missing values, but when the whole
-##' vector is missing it returns NA rather than zero.
+##' vector is missing it returns NA rather than zero. This function is
+##' merely provided so that it does not return unexpected result for
+##' users.x
 ##'
-##' @param x The vector to be summed.
+##' @param ... The vector to be summed, see the base::sum function.
+##' @param na.rm Same as base::sum
 ##'
 ##' @export
 ##' 
