e6c56306a72c044c9fc653f48e020d19a91ca95c
rockclimber112358 <rockclimber112358@gmail.com>
2015-04-20 09:39:42 +0200

    Adding documentation, data check to adjacent2edge function

--- codes/adjacent2edge.R
+++ codes/adjacent2edge.R
@@ -1,13 +1,22 @@
 ##' Function to convert tree in adjacent list format to edge
 ##' representation.
 ##'
-##' @param tree The graph or tree in adjacent list format.
+##' @param tree A data.table object with two columns.  The first column should
+##' have the parent nodes and the second column should have a character vector
+##' containing all the children nodes separated by commas (i.e. adjacent list
+##' format).
+##'
+##' @return A data.table object containing two columns: parent and children.
 ##'
 ##' @export
 ##' 
 
 
 adjacent2edge = function(tree){
+    
+    ## Data Quality Checks
+    stopifnot(is(tree, "data.table"))
+    
     children = strsplit(unlist(tree[, 2, with = FALSE]), ", ")
     data.table(parent = rep(unlist(tree[, 1, with = FALSE]),
                    sapply(children, length)),
